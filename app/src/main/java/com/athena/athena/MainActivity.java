322d7e67742a6f1466b75aa4c6c2cf21b0ec112d
montarion <jamirograntsaan@gmail.com>
2019-10-21 17:38:47 +0200

    added all base visual elements

--- app/src/main/java/com/athena/athena/MainActivity.java
+++ app/src/main/java/com/athena/athena/MainActivity.java
@@ -1,13 +1,22 @@
 package com.athena.athena;
 
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final String TAG = "main";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View calview = (View) findViewById(R.id.rect_calend);
+        
+
     }
 }
